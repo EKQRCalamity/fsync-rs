5f020ef550736d2645e6050cebc664683ca5e12f
Kain (Pseudonym) <88363187+EKQRCalamity@users.noreply.github.com>
2023-06-18 00:56:38 +0200

    Argument Management and Dirty Pass Thru

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,6 @@ use std::fs::File;
 use std::io::{BufReader, Read};
 
 
-const DATE_FORMAT_STR: &'static str = "%d-%m-%Y";
-
-
 /// THANKS https://stackoverflow.com/questions/69787906/how-to-hash-a-binary-file-in-rust
 fn sha256_digest(path: &PathBuf) -> io::Result<String> {
     let input = File::open(path)?;
@@ -35,21 +32,70 @@ fn sha256_digest(path: &PathBuf) -> io::Result<String> {
     Ok(HEXLOWER.encode(digest.as_ref()))
 }
 
+struct ArgPassThru {
+    format: String,
+    format_out: String
 
-fn copy_files(source_dir: &Path, destination_dir: &Path) -> io::Result<bool> {
-    
-    let args: Vec<_> = env::args().collect();
+}
+
+impl ArgPassThru {
+    pub fn new(format: String, format_out: String) -> ArgPassThru {
+        return ArgPassThru { format: format, format_out: format_out };
+    }
+
+    fn setformat(&mut self, format: String) {
+        self.format = format;
+    }
+
+    fn setformat_out(&mut self, format: String) {
+        self.format_out = format;
+    }
+}
+
+fn handleargs(args: &mut[String]) -> ArgPassThru {
+    let mut pass: ArgPassThru = ArgPassThru::new("".to_string(), "".to_string());
     if args.len() > 1 {
-        if args[1] == "-c" {
-            configconstructor();
+        let mut n: usize = 0;
+        for argument in args {
+            if argument == "-h" {
+                println!("Following arguments are passable:\n -h            Shows this message.\n -c            Re-Enter config setup.\n -format       Change the source directory date format. (Default is none)\n -format_out   Change the output date format. (Default is none)\n -dformat      Sets the format and format_out to my defaults.");
+                std::process::exit(0);
+            } else if argument == "-c" {
+                configconstructor();
+            } else if argument == "--format" {
+                let format = env::args().nth(n + 1).unwrap();
+                if env::args().nth(n + 1).is_none() || format.is_empty() {
+                    panic!("Command line parameter after --format must be passed.");
+                } else {
+                    pass.setformat(format);
+                }
+            } else if argument == "--format_out" {
+                let format = env::args().nth(n + 1).unwrap();
+                if env::args().nth(n + 1).is_none() || format.is_empty() {
+                    panic!("Command line parameter after --format must be passed.");
+                } else {
+                    pass.setformat_out(format);
+                }
+            } else if argument == "--dformat" {
+                pass.setformat("%Y-%m-%d".to_string());
+                pass.setformat_out("%d-%m-%Y".to_string());
+            }
+            n += 1;
         }
     }
-    
+    return pass;
+}
+
+fn copy_files(source_dir: &Path, destination_dir: &Path) -> io::Result<bool> {
     let mut changed_files = false;
 
     if !destination_dir.exists() {
         fs::create_dir(destination_dir)?;
         println!("Missing directory {} created", destination_dir.display());
+    }
+    if !source_dir.exists() {
+        fs::create_dir(source_dir)?;
+        println!("Missing directory {} created", source_dir.display());
     }
 
     // Iterate over the entries in the source directory
@@ -98,14 +144,19 @@ fn copy_files(source_dir: &Path, destination_dir: &Path) -> io::Result<bool> {
 }
 
 fn main() {
+    let mut args: Vec<_> = env::args().collect();
+    let argpasser = handleargs(args.as_mut_slice());
     let conf = getconfig();
-    let source_dir = Path::new(&conf.source_dir);
+    let bdate = Local::now();
+    let bsource_dir = format!("{}{}", conf.source_dir, bdate.format(&argpasser.format));
     
-    println!("Starting to listen to changes in folder {}", source_dir.display());
+    println!("Starting to listen to changes in folder {}", bsource_dir);
 
     loop {
         let date = Local::now();
-        let destination_dir = format!("{}-{}\\", conf.base_out_dir, date.format(DATE_FORMAT_STR));
+        let source_dir = format!("{}{}", conf.source_dir, date.format(&argpasser.format));
+        let source_dir = Path::new(&source_dir);
+        let destination_dir = format!("{}{}\\", conf.base_out_dir, date.format(&argpasser.format_out));
         let destination_dir = Path::new(&destination_dir);
 
         match copy_files(&source_dir, &destination_dir) {
